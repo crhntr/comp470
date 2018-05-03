0249a3532cc6ea72a7311c9be43bd8800b2dea86
crhntr <hunter.christopher@icloud.com>
2018-05-03 12:17:28 -0700

    Some whitespace stuff

--- finalBotCode/finalBotCode.ino
+++ finalBotCode/finalBotCode.ino
@@ -14,43 +14,43 @@ const double PIE_O2 = PIE/2.0;
 const double PIE2   = PIE*2.0;
 
 
-//-------- motor control 
+//-------- motor control
 
 void TurnLeft90();
 void TurnRight90();
 void Straight( int speed, int dirn );
 void UpdateLoc();
 
-//-------- dead reckoning 
+//-------- dead reckoning
 
 // ticks per rotation
 #define TPR 72
- 
+
 // robot measurements (mm)
 #define RW    42.5  // radius wheel
 #define D     158.0
 
 // robot config variables
-double x = 100.0, 
-       y = 100.0, 
-       dx = 0.0, 
+double x = 100.0,
+       y = 100.0,
+       dx = 0.0,
        dy = 0.0;
 double theta =  PI/2.0;
 
 // encoder variables
-volatile long left_encoder_count = 0, 
-              right_encoder_count = 0;   
-int left_dirn = 1, 
+volatile long left_encoder_count = 0,
+              right_encoder_count = 0;
+int left_dirn = 1,
     right_dirn = 1;
 
-//-------- model of environment 
+//-------- model of environment
 
 double LEFT = 0.0;
 double RIGHT = 1500.0;
 double BOTTOM = 0.0;
 double TOP = 1500.0;
 
-float positionX = 0.0, 
+float positionX = 0.0,
       positionY = 0.0;
 
 float map[7][7] = {
@@ -100,61 +100,61 @@ void loop() {
 void TurnLeft90()
 {
     right_encoder_count = left_encoder_count = 0;
-    
-    left_dirn = -1; 
+
+    left_dirn = -1;
     right_dirn = 1;
-    
-    MOTOR.setSpeedDir1(40, DIRR); 
-    MOTOR.setSpeedDir2(40, DIRR); 
-    
+
+    MOTOR.setSpeedDir1(40, DIRR);
+    MOTOR.setSpeedDir2(40, DIRR);
+
     while (right_encoder_count < 64)
     {
       delayMicroseconds(1);
     }
 
-    MOTOR.setSpeedDir1(0, DIRF); 
+    MOTOR.setSpeedDir1(0, DIRF);
     MOTOR.setSpeedDir2(0, DIRR);
 }
 
 void TurnRight90()
 {
     right_encoder_count = left_encoder_count = 0;
-    
-    left_dirn = 1; 
+
+    left_dirn = 1;
     right_dirn = -1;
-    
-    MOTOR.setSpeedDir1(40, DIRF); 
-    MOTOR.setSpeedDir2(40, DIRF); 
+
+    MOTOR.setSpeedDir1(40, DIRF);
+    MOTOR.setSpeedDir2(40, DIRF);
     while (left_encoder_count < 64)
     {
       delayMicroseconds(1);
     }
 
-    MOTOR.setSpeedDir1(0, DIRF); 
+    MOTOR.setSpeedDir1(0, DIRF);
     MOTOR.setSpeedDir2(0, DIRR);
 }
 
 void Straight( int speed, int dirn )
 {
     //---- setup encoder variables
-    left_dirn = dirn; 
+    left_dirn = dirn;
     right_dirn = dirn;
-    
+
     if (speed == 0)       //-- stop
     {
-      MOTOR.setSpeedDir1(0, DIRF); 
-      MOTOR.setSpeedDir2(0, DIRR);   
+      MOTOR.setSpeedDir1(0, DIRF);
+      MOTOR.setSpeedDir2(0, DIRR);
       return;
     }
     else if (dirn == 1)   //-- fwd
     {
-      MOTOR.setSpeedDir1(speed+0.25*speed, DIRF); 
-      MOTOR.setSpeedDir2(speed, DIRR); 
+      MOTOR.setSpeedDir1(speed+0.25*speed, DIRF);
+      MOTOR.setSpeedDir2(speed, DIRR);
     }
     else                  //-- bwd
     {
-      MOTOR.setSpeedDir1(speed+0.25*speed, DIRR); 
-      MOTOR.setSpeedDir2(speed, DIRF); 
+      MOTOR.setSpeedDir1(speed+0.25*speed, DIRR);
+      MOTOR.setSpeedDir2(speed, DIRF);
     }
 }
 
@@ -162,10 +162,10 @@ void UpdateLoc()
 {
   dx = PIE * RW * cos(theta) * ((double)(left_encoder_count + right_encoder_count) / TPR);
   x = x + dx;
-  
+
   dy = PIE * RW * sin(theta) * ((double)(left_encoder_count + right_encoder_count) / TPR);
   y = y + dy;
-  
+
   right_encoder_count = left_encoder_count = 0;
 }
 
@@ -181,6 +181,3 @@ void RightEncoder()
 {
   right_encoder_count = right_encoder_count + right_dirn;
 }
-
-
-
